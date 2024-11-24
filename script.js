ac043c9bf330f43169abae4f47f19ec1904d8c35
sssseaurchin <157834865+sssseaurchin@users.noreply.github.com>
2024-11-24 18:27:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-function validatePhone() {
-    var phoneInput = document.getElementById('phone').value;
-    var phonePattern = /^\+90\s\d{3}\s\d{3}\s\d{2}\s\d{2}$/;
+const apiURL = 'https://run.mocky.io/v3/8818bd3c-d3c0-44f3-8e7a-1dd0c1cf2c2c';
+
+async function fetchData() {
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+
+        const courses = data.courses;
+        const cities = data.cities;
+
+        const coursesDropdown = document.getElementById('courses');
+        const citiesDropdown = document.getElementById('cities');
+
+        let counter = 1;
+
+        courses.forEach(course => {
+            const option = document.createElement('option');
+            option.value = counter;
+            option.textContent = course;
+            coursesDropdown.appendChild(option);
+
+            counter++;
+        });
+
+        counter = 1;
+
+        cities.forEach(city => {
+            const option = document.createElement('option');
+            option.value = counter;
+            option.textContent = city;
+            citiesDropdown.appendChild(option);
+
+            counter++;
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+function validatePhone(event) {
+    var phoneInput = document.getElementById('phone').value.trim().replace(/\s/g, '');
+
+    var phonePattern = /^5[0-9]{9}$/;
 
     if (!phonePattern.test(phoneInput)) {
-        alert('Please enter a valid Turkish phone number in the format: +90 555 123 45 67');
+        alert('Please enter a valid Turkish phone number in the format: 5XX XXX XX XX');
+        event.preventDefault();
     }
 }
 
-function closeAd() {
-    document.getElementById('ad').style.display = 'none';
+function validateEmail(event) {
+    var emailInput = document.getElementById('email').value;
+    var emailPattern = /^[a-z0-9]+@[a-z0-9-]+\.[a-z]{2,}(\.[a-z]{2,})?$/;
+
+    if (!emailPattern.test(emailInput)) {
+        alert('Please enter a valid email address');
+        event.preventDefault();
+    }
 }
+function checkDropDownSelection(event){
+        const dropdown1 = document.getElementById('courses');
+        const dropdown2 = document.getElementById('cities');
+
+        if (dropdown1.value == '0' || dropdown2.value == '0') {
+            alert('Please select valid options from both courses and cities dropdowns.');
+            event.preventDefault();
+        }
+}
+
+// function closeAd() {
+//     document.getElementById('ad').style.display = 'none';
+// }
